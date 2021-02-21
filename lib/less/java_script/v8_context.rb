55db6083dc1741238a88f6a95dce97fe8883c938
Raul Salamanca <ralsalamanca@gmail.com>
2021-02-21 15:03:20 -0300

    replace therubyracer to mini_racer in v8 context

--- lib/less/java_script/v8_context.rb
+++ lib/less/java_script/v8_context.rb
@@ -1,14 +1,25 @@
 begin
-  require 'v8' unless defined?(V8)
+  require 'mini_racer' unless defined?(MiniRacer)
 rescue LoadError => e
-  warn "[WARNING] Please install gem 'therubyracer' to use Less."
+  warn "[WARNING] Please install gem 'mini_racer' to use Less."
   raise e
 end
 
-require 'pathname'
-
 module Less
   module JavaScript
+    
+    class ExternalMiniRacer < MiniRacer::Context
+      
+      def [](key)
+        @functions[key]
+      end
+
+      def []=(key, val)
+        @functions[key] = val
+      end
+
+    end
+
     class V8Context
 
       def self.instance
@@ -16,39 +27,37 @@ module Less
       end
 
       def initialize(globals = nil)
-        lock do
-          @v8_context = V8::Context.new
-          globals.each { |key, val| @v8_context[key] = val } if globals
-        end
+        @mini_context = ExternalMiniRacer.new
+        globals.each { |key, val| @mini_context[key] = val } if globals
       end
 
       def unwrap
-        @v8_context
+        @mini_context
       end
 
       def exec(&block)
         lock(&block)
       end
 
-      def eval(source, options = nil) # passing options not supported
+      def eval(source, options = nil)
         source = source.encode('UTF-8') if source.respond_to?(:encode)
 
         lock do
-          @v8_context.eval("(#{source})")
+          @mini_context.eval("(#{source})")
         end
       end
 
       def call(properties, *args)
-        args.last.is_a?(::Hash) ? args.pop : nil # extract_options!
+        args.last.is_a?(::Hash) ? args.pop : nil
 
         lock do
-          @v8_context.eval(properties).call(*args)
+          @mini_context.eval(properties).call(*args)
         end
       end
 
       def method_missing(symbol, *args)
-        if @v8_context.respond_to?(symbol)
-          @v8_context.send(symbol, *args)
+        if @mini_context.respond_to?(symbol)
+          @mini_context.send(symbol, *args)
         else
           super
         end
@@ -58,39 +67,27 @@ module Less
 
         def lock(&block)
           do_lock(&block)
-        rescue V8::JSError => e
-          if e.in_javascript?
-            js_value = e.value.respond_to?(:'[]')
-            name = js_value && e.value["name"]
-            constructor = js_value && e.value['constructor']
-            if name == "SyntaxError" ||
-                ( constructor && constructor.name == "LessError" )
-              raise Less::ParseError.new(e, js_value ? e.value : nil)
-            end
-          # NOTE: less/parser.js :
-          #
-          #   error = new(LessError)({
-          #      index: i,
-          #      type: 'Parse',
-          #      message: "missing closing `}`",
-          #      filename: env.filename
-          #   }, env);
-          #
-          # comes back as value: RuntimeError !
-          elsif e.value.to_s =~ /missing opening `\(`/
-            raise Less::ParseError.new(e.value.to_s)
+        rescue MiniRacer::RuntimeError => e
+          if e.value && ( e.value['message'] || e.value['type'].is_a?(String) )
+            raise Less::ParseError.new(e, e.value)
           end
-          raise Less::Error.new(e)
+          if e.unwrap.to_s =~ /missing opening `\(`/
+            raise Less::ParseError.new(e.unwrap.to_s)
+          end
+          if e.message && e.message[0, 12] == "Syntax Error"
+            raise Less::ParseError.new(e)
+          else
+            raise Less::Error.new(e)
+          end
         end
 
         def do_lock
           result, exception = nil, nil
-          V8::C::Locker() do
-            begin
-              result = yield
-            rescue Exception => e
-              exception = e
-            end
+          
+          begin
+            result = yield
+          rescue Exception => e
+            exception = e
           end
 
           if exception
